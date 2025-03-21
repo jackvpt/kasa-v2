57280fc09a94219adc333a39de5073a68f866040
Jacques Verpoest <jacques.verpoest@gmail.com>
2025-03-21 23:02:20 +0100

    minor improvements

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,17 +1,23 @@
 import React from "react"
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom"
 import "./Error.scss"
 
 export default function Error() {
   return (
     <section className="container__error">
       <h1>404</h1>
-      <h2>Oups! La page que vous demandez n'existe pas.</h2>
-      <Link className="container__error-link" to="/"> {/* Link to Home page */}
-				Retourner sur la page d'accueil
-			</Link>
+      <h2>
+        Oups! La page que
+        <span className="span__crlf_404">
+          <br />
+        </span>{" "}
+        vous demandez n'existe pas.
+      </h2>
+      <Link className="container__error-link" to="/">
+        {" "}
+        {/* Link to Home page */}
+        Retourner sur la page d'accueil
+      </Link>
     </section>
   )
 }
-
-
